b9e156975c61b72bea3f7910af1adb875b5bd102
Christopher Miles <cmilespv@gmail.com>
2026-09-07 22:15:23 -0500

    got a triangle on a silly draw function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame 
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
@@ -8,12 +10,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player(x = SCREEN_WIDTH/ 2, y = SCREEN_HEIGHT / 2)
+
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
