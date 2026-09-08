9ab878b810f2129b6d7c8250a16a2f4fe298fd68
Christopher Miles <cmilespv@gmail.com>
2026-09-07 23:02:34 -0500

    establishing Group structure

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH/ 2, y = SCREEN_HEIGHT / 2)
 
+    #Starting Game Loop
     while (True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
