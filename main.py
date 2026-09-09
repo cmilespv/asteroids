eb06a288f3c2524afc28386dd887f29da23dce4f
Christopher Miles <cmilespv@gmail.com>
2026-09-09 17:54:33 -0500

    added the field display of asteroids

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
@@ -12,8 +14,12 @@ def main():
     dt = 0.0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH/ 2, y = SCREEN_HEIGHT / 2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers =(updatable)
+    asteroidfield = AsteroidField()
 
     #Starting Game Loop
     while (True):
